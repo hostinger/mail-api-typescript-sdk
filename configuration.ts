c2d435e1553204217bc7ff1c3cb3cb20b05ebd8a
aurimasbutkus <aurimasbutkus@users.noreply.github.com>
2026-08-11 14:21:16 +0000

    chore(deps-dev): bump @types/node from 26.1.2 to 26.2.0 in /deps/typescript-axios (#123)
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- configuration.ts
+++ configuration.ts
@@ -48,7 +48,7 @@ export class Configuration {
         this.baseOptions = {
             ...param.baseOptions,
             headers: {
-                'User-Agent': "hostinger-mail-typescript-sdk/1.17.2",
+                'User-Agent': "hostinger-mail-typescript-sdk/1.17.3",
                 ...param.baseOptions?.headers,
             },
         };
